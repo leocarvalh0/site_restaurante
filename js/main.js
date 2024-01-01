a5fc4604612bf4e3d4f79dcb2c712b87fbeb367b
Leonardo Carvalho <0leonardocarvalho@gmail.com>
2024-01-01 12:44:03 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            }
+        }, 
+
+        submitHandler: function(form) {
+            console.log(form);
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+})
+})
